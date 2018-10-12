0ff247a652e35746b6e591722ad0ed9224cbfbd7
Tim Broughton <TimBroughton@quickenloans.com>
2018-10-12 15:53:15 -0400

    initial commit

--- Bond James (Tim Broughton)/Program.cs
+++ Bond James (Tim Broughton)/Program.cs
@@ -10,6 +10,37 @@ namespace Bond_James__Tim_Broughton_
     {
         static void Main(string[] args)
         {
+
+            int userNumber;
+            string userInput;
+
+            while (true)
+            {
+                Console.Write("Please enter a number between 0 and 12: ");
+
+
+                userInput = Console.ReadLine();
+
+                userNumber = Int32.Parse(userInput);
+
+
+                if (userNumber > 12 || userNumber < 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+                for (int i = 0; i < 13; i++)
+                {
+                    Console.WriteLine("{0} X {1} = {2}\n", i, userNumber, (i * userNumber));
+                }
+
+            Console.ReadKey();
+
         }
     }
 }
